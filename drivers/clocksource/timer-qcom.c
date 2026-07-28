e4213550b0aa645c47c7a10ec76aacf269717c4c
j0sh1x <aljoshua.hell@gmail.com>
2026-07-28 17:08:42 +0200

    drivers: clocksource: timer-qcom: cleanup

--- drivers/clocksource/timer-qcom.c
+++ drivers/clocksource/timer-qcom.c
@@ -16,8 +16,7 @@
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/sched_clock.h>
-
-#include <asm/delay.h>
+#include <linux/delay.h>
 
 #define TIMER_MATCH_VAL			0x0000
 #define TIMER_COUNT_VAL			0x0004
